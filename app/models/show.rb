7925001a51ddecc9bba265eae6d400083ca1547b
Jonathan East <jonathan_east@ymail.com>
2020-07-14 20:03:37 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -11,4 +11,8 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
     Show.minimum(:rating)
   end
+
+  def self.most_popular_show
+    Show.where("rating = ?", self.lowest_rating).first
+  end
 end
